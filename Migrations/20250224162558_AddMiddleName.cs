03301956709ea01ef5a04636da8be676ff072ee1
Tomáš Dračka <tomidracka@gmail.com>
2025-02-24 17:30:43 +0100

    - Add MiddleName property to Student entity

---
+++ Migrations/20250224162558_AddMiddleName.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMiddleName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MiddleName",
+                table: "Students",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MiddleName",
+                table: "Students");
+        }
+    }
+}
---
+++ Scripts/V2__AddMiddleName.sql
@@ -0,0 +1,9 @@
+﻿START TRANSACTION;
+
+ALTER TABLE `Students` ADD `MiddleName` longtext CHARACTER SET utf8mb4 NOT NULL;
+
+INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+VALUES ('20250224162558_AddMiddleName', '8.0.4');
+
+COMMIT;
+
