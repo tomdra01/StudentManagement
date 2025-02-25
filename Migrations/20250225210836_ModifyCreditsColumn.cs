aa336612c79263a023660bedd7fcbdf1c6a627af
Tomáš Dračka <tomidracka@gmail.com>
2025-02-25 22:11:47 +0100

    - Change Credits property type from int to decimal in Course

---
+++ Migrations/20250225210836_ModifyCreditsColumn.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyCreditsColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Credits",
+                table: "Courses",
+                type: "decimal(5,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Credits",
+                table: "Courses",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(5,2)");
+        }
+    }
+}
---
+++ Scripts/V7__ModifyCredits.sql
@@ -0,0 +1,9 @@
+﻿START TRANSACTION;
+
+ALTER TABLE `Courses` MODIFY COLUMN `Credits` decimal(5,2) NOT NULL;
+
+INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+VALUES ('20250225210836_ModifyCreditsColumn', '8.0.4');
+
+COMMIT;
+
